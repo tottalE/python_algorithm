a222788c9559ee0d461cb67ef9454225046535a6
jin <pearlisrich@naver.com>
2024-04-13 15:32:59 +0900

    :memo: update readme.md

--- jinju/03_문자열_뒤집기.py
+++ jinju/03_문자열_뒤집기.py
@@ -1,4 +1,7 @@
 input_str = input()
+# "010011" -> " 1  11" -> ["1", "11"]
+# "0 00  " -> ["0", "00"]
+
 result = min(len(input_str.replace("0", " ").split()), len(input_str.replace("1", " ").split()))
 
 print(result)
