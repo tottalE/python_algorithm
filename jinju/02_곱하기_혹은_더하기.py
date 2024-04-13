153ab515f00ea820c525c758d6175d07f0c38ef4
jin <pearlisrich@naver.com>
2024-04-13 14:55:30 +0900

    :sparkles: add 02_곱하기_혹은_더하기.py

---
+++ jinju/02_곱하기_혹은_더하기.py
@@ -0,0 +1,11 @@
+arr = list(input())
+arr_int = list(map(int, arr))
+result = arr_int[0]
+
+for i in range(1, len(arr)):
+    if arr_int[i-1] > 1 and arr_int[i] > 1:
+        result *= arr_int[i]
+    else:
+        result += arr_int[i]
+
+print(result)
