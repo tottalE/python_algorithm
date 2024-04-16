c0de0e45814f46e4a527dcda047d5c5dab8a4d2d
AyJeong <68263463+AyJeong@users.noreply.github.com>
2024-04-16 17:56:02 +0900

    Q4 update

--- ajeong/04_만들_수_없는_금액.py
+++ ajeong/04_만들_수_없는_금액.py
@@ -1,11 +1,36 @@
-from itertools import combinations
-
 N = int(input())
 
 coins = list(map(int, input().split()))
 
 coins.sort()
 
+cost = []
 
 #조합을 구해야하나...?
-#1부터 키워가면서 만들 수 없으면 그냥 종료 시키는건 너무 위험..?
+for i in coins:
+    cost.append(i)
+
+#2개로 만들 수 있는 금액
+for i in range(N-1):
+    for j in (i+1, N-1, 1):
+        temp = coins[i]+coins[j]
+        if temp not in cost:
+            cost.append(temp)
+
+print(cost)
+
+cost.sort()
+
+answer = 0
+for i in range(1, len(cost)+1, 1):
+    if i not in cost:
+        answer = i
+        break
+
+if answer == 0:
+    answer = cost[-1]+1
+    
+print(answer)
+
+#2개로 만들 수 있는 금액
+#조합을 다 구하지 않고도 작은 값이
