a9a3f491026ae9e96863082ddcbb9a19a939ba90
tottal <66786418+tottalE@users.noreply.github.com>
2024-05-02 18:12:39 +0900

    Update 0427_best_time_to_buy.py

--- jinju/0427_best_time_to_buy.py
+++ jinju/0427_best_time_to_buy.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profits = [0]
         min_num = 100001
+        max_profit = 0
 
         for i in range(len(prices)):
             if prices[i] < min_num:
                 min_num = prices[i]
             elif prices[i] > min_num:
-                profits.append(prices[i] - min_num)
+                max_profit = max(max_profit, prices[i] - min_num)
 
-
-        return max(profits)
+        return max_profit
