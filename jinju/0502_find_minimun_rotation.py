61078339dc6baf3b7a58dac6680fe0baa5c03d8d
tottal <66786418+tottalE@users.noreply.github.com>
2024-05-03 10:35:59 +0900

    Update 0502_find_minimun_rotation.py

--- jinju/0502_find_minimun_rotation.py
+++ jinju/0502_find_minimun_rotation.py
@@ -1,17 +1,17 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums)-1
+        result = nums[0]
+        l, r = 0, len(nums) - 1
 
-        if nums[0] < nums[-1]:
-            return nums[0]
-
-        while r-l>1:
-            if nums[r] > nums[r-1] and nums[l] < nums[l+1]:
-                l += 1
-                r -= 1
-            elif nums[r] > nums[r-1]:
-                r -= 1
-            elif nums[l] < nums[l+1]:
-                l += 1
-        
-        return nums[r]
+        while l <= r:
+            if nums[l] < nums[r]:
+                result = min(result, nums[l])
+                break
+                
+            m = (l+r) // 2
+            result = min(result, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        return result
